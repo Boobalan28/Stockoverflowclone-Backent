6c28aa1bc94c55d087169ca3b99580289243c78f
Boobalan/28 <boobal7722@gmail.com>
2022-01-11 13:04:07 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+//requiring dependencies.
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json())
+app.use(cors());
+
+const UserRouter = require('./Router/user.router');
+const QuestionRouter = require('./Router/question.router');
+app.use('/users', UserRouter);
+app.use('/questions', QuestionRouter)
+
+app.get("/",(req,res)=>{
+    res.send("server working");
+});
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
+
+const mongoURL="mongodb+srv://Boobalan:boobalan1234@cluster0.ixzoi.mongodb.net/Stockoverflow-clone";
+mongoose.connect(mongoURL, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
